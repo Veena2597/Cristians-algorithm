1447ea46beb73c29cbd08931753642ab913e6de0
Veena2597 <nbellamkonda@ucsb.edu>
2020-10-13 20:55:08 -0700

    Added comments

--- client.py
+++ client.py
@@ -27,34 +27,38 @@ def send(message):
 
 
 def synchronizeTime():
+    # Client sends CLOCK_REQUEST to the clock server and the request time is recorded
     request_time = default_timer()
     client.send(CLOCK_REQUEST.encode(FORMAT))
-    print("[CLOCK SERVER] Requested time from server")
+    print(f"[CLOCK SERVER] Requested time from server at {request_time}")
 
+    # Client receives time from the clock server and the response time is recorded
     clock_server_time = parser.parse(client.recv(1024).decode(FORMAT))
     response_time = default_timer()
     actual_time = datetime.datetime.now()
-
     print(f"[CLOCK SERVER] Time received from clock server {str(clock_server_time)}")
 
+    # Delay between request sent and response received is calculated
     delay = response_time - request_time
     print(f"[CLIENT CLOCK] Delay in response from clock server is {str(delay)}s")
-    print(f"[CLIENT CLOCK] Time before synchronization {str(actual_time)}s")
 
-    client_time = clock_server_time + datetime.timedelta(seconds=(delay) / 2)
+    # Times before and after synchronization are printed
+    print(f"[CLIENT CLOCK] Time before synchronization {str(actual_time)}s")
+    client_time = clock_server_time + datetime.timedelta(seconds= (delay)/2)  # Calculated with Cristian's algorithm
     print(f"[CLIENT CLOCK] Time after synchronization {str(client_time)}s")
 
     error = actual_time - client_time
     print(f"[CLIENT CLOCK] Synchronization error {str(error.total_seconds())}s")
 
+    # The client ping the clock again after 20s
     time.sleep(20)
 
 
 if __name__ == '__main__':
     client.connect(ADDRESS)
     while True:
+        # Client connects to the clock server and a thread is created for synchronizing time
         clock_thread = threading.Thread(target=synchronizeTime())
         clock_thread.start()
-    # send("Connection Secure")
-    # send(DISCONNECT_MESSAGE)
+
     client.close()
--- clock_server.py
+++ clock_server.py
@@ -13,24 +13,32 @@ CLOCK_REQUEST = "SYNCHRONIZE"
 def sendTime(connection, address):
     connected = True
     while connected:
+        # Client sends CLOCK_REQUEST whenever it needs to know the time from the server
         msg = connection.recv(1024).decode(FORMAT)
         if msg == CLOCK_REQUEST:
             connection.send(str(datetime.datetime.now()).encode(FORMAT))
+
     connection.close()
 
 
 if __name__ == '__main__':
+    # Creating the clock server socket
     clock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("f[CLOCK] Successfully created")
+    print("f[CLOCK SERVER] Successfully created")
+
+    # Binding the clock server socket to the address - defined globally
     clock.bind(ADDRESS)
 
+    # The clock server keeps listening for different clients
     clock.listen()
-    print("f[CLOCK LISTEN] Clock server is listening")
+    print("f[CLOCK SERVER] Clock server is listening")
 
     while True:
+        # Clock server accepts new clients and creates a new thread for each client
         connection, address = clock.accept()
-        print(f"[NEW CONNECTION] {address} connected")
+        print(f"[CLOCK SERVER]  New client connected with address: {address}")
         thread = threading.Thread(target=sendTime, args=(connection, address))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+        print(f"[CLOCK SERVER] Current number of clients connected: {threading.active_count() - 1}")
+
     clock.close()
